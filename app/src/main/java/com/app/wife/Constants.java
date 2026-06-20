57ce1f40c62b7e85076c9027c0c7613e5f9dc0a0
josyvine <64621103+josyvine@users.noreply.github.com>
2026-06-20 22:11:05 +0530

    Update Constants.java

--- app/src/main/java/com/app/wife/Constants.java
+++ app/src/main/java/com/app/wife/Constants.java
@@ -9,6 +9,10 @@ public final class Constants {
     public static final int OFF_PORT_FILE = 8900;
     public static final int OFF_PORT_VOICE = 8901;
     public static final int OFF_PORT_VIDEO = 8902;
+    
+    // Decoupled group call UDP ports to maintain parallel pipelines
+    public static final int OFF_PORT_GROUP_AUDIO = 8903;
+    public static final int OFF_PORT_GROUP_VIDEO = 8904;
 
     // Intents and extras keys
     public static final String EXTRA_PEER_IP = "com.wife.app.EXTRA_PEER_IP";
@@ -27,11 +31,20 @@ public final class Constants {
     public static final String SIGNAL_VIDEO_REJECT = "VIDEO_CALL_REJECT";
     public static final String SIGNAL_VIDEO_END = "VIDEO_CALL_END";
 
+    // Decoupled group call signaling events
+    public static final String SIGNAL_GROUP_CALL_REQUEST = "GROUP_CALL_REQUEST";
+    public static final String SIGNAL_GROUP_CALL_ACCEPT = "GROUP_CALL_ACCEPT";
+    public static final String SIGNAL_GROUP_CALL_REJECT = "GROUP_CALL_REJECT";
+    public static final String SIGNAL_GROUP_CALL_END = "GROUP_CALL_END";
+
     // Broadcast actions
     public static final String ACTION_CONNECTION_CHANGED = "com.wife.app.ACTION_CONNECTION_CHANGED";
     public static final String ACTION_PEER_DISCOVERED = "com.wife.app.ACTION_PEER_DISCOVERED";
     public static final String ACTION_CALL_RECEIVED = "com.wife.app.ACTION_CALL_RECEIVED";
     public static final String ACTION_VIDEO_CALL_RECEIVED = "com.wife.app.ACTION_VIDEO_CALL_RECEIVED";
+    
+    // Decoupled group call broadcast action
+    public static final String ACTION_GROUP_CALL_RECEIVED = "com.wife.app.ACTION_GROUP_CALL_RECEIVED";
     
     public static final String EXTRA_CALL_SENDER = "EXTRA_CALL_SENDER";
     public static final String EXTRA_CALL_IP = "EXTRA_CALL_IP";
